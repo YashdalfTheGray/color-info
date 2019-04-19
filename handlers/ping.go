507bd3c22a816753647e983ed1e97717426cf792
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-04-19 15:25:42 -0700

    added a ping handler to the server

---
+++ handlers/ping.go
@@ -0,0 +1,13 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// PingHandler responds with string "pong" when GET /ping
+func PingHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "pong")
+}
--- handlers/status.go
+++ handlers/status.go
@@ -9,7 +9,7 @@ import (
 
 // NewStatusHandler configures a status handler with a server id
 // and returns it. The status handler will respond with a JSON
-// { status: "ok", serverId: "something" }
+// { status: "ok", serverId: "some string" }
 func NewStatusHandler(serverID string) func(w http.ResponseWriter, req *http.Request) {
 	return func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
